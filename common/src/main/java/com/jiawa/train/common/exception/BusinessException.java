e192e26d2d45a093bbfac7582e78d43b1ed50bde
buxiangqi <3160231312@qq.com>
2024-11-30 14:27:03 +0800

    4.18 使用自定义异常处理异常业务, 不写入堆栈信息, 提高性能

--- common/src/main/java/com/jiawa/train/common/exception/BusinessException.java
+++ common/src/main/java/com/jiawa/train/common/exception/BusinessException.java
@@ -15,4 +15,9 @@ public class BusinessException extends RuntimeException {
     public void setE(BusinessExceptionEnum e) {
         this.e = e;
     }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
 }
